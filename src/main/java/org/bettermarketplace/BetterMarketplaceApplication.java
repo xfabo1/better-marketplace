4a7128b18ad1982603a38f42f48511c0b85e994a
xfabo1 <filip.fabry16@gmail.com>
2024-12-04 09:53:56 +0100

    Initial commit

---
+++ src/main/java/org/bettermarketplace/BetterMarketplaceApplication.java
@@ -0,0 +1,14 @@
+package org.bettermarketplace;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterMarketplaceApplication {
+
+	public static void main(String[] args) {
+		run(BetterMarketplaceApplication.class, args);
+	}
+
+}
