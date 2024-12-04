957b78f1d3f89aaa524372cdce9c6f3233a84476
xfabo1 <filip.fabry16@gmail.com>
2024-12-04 09:53:56 +0100

    Initial commit

---
+++ src/test/java/org/bettermarketplace/BetterMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bettermarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
