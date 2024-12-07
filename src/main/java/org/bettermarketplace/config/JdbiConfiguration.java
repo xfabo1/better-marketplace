454d98c14519ba9e339106c3e47576a62b8651ea
xfabo1 <filip.fabry16@gmail.com>
2024-12-07 10:00:13 +0100

    Feat: Implement the database access layer

--- src/main/java/org/bettermarketplace/config/JdbiConfiguration.java
+++ src/main/java/org/bettermarketplace/config/JdbiConfiguration.java
@@ -1,8 +1,12 @@
 package org.bettermarketplace.config;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.jackson2.Jackson2Plugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +15,11 @@ import org.springframework.context.annotation.Configuration;
 public class JdbiConfiguration {
 
 	@Bean
-	public Jdbi jdbi(DataSource dataSource) {
-		return Jdbi.create(dataSource).installPlugin(new SqlObjectPlugin());
+	public Jdbi jdbi(DataSource dataSource, List<RowMapper<?>> rowMappers) {
+		var jdbi = Jdbi.create(dataSource);
+		jdbi.installPlugin(new SqlObjectPlugin());
+		jdbi.installPlugin(new Jackson2Plugin());
+		rowMappers.forEach(jdbi::registerRowMapper);
+		return jdbi;
 	}
 }
