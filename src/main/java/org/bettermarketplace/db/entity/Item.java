556d2a3ef7b2dced387a2bf08fd6ffcee6bff6db
xfabo1 <filip.fabry16@gmail.com>
2024-12-08 13:51:43 +0100

    feat: Setup the structure for the project

---
+++ src/main/java/org/bettermarketplace/db/entity/Item.java
@@ -0,0 +1,14 @@
+package org.bettermarketplace.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+	private String id;
+	private String name;
+}
