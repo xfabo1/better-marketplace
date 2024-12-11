9050f34c1942c9bef421972e5793d7f67c191ff2
xfabo1 <filip.fabry16@gmail.com>
2024-12-11 11:52:53 +0100

    feat: Change the migration and the structure of the project

--- src/it/java/org/bettermarketplace/configuration/PostgisTest.java
+++ src/it/java/org/bettermarketplace/configuration/PostgisTest.java
@@ -1,14 +1,11 @@
 package org.bettermarketplace.configuration;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
-public class PostgisTest {
+@ActiveProfiles("postgres")
+public abstract class PostgisTest {
 }
--- src/main/java/org/bettermarketplace/api/dto/OpenedFileDto.java
+++ src/main/java/org/bettermarketplace/api/dto/OpenedFileDto.java
@@ -2,33 +2,17 @@ package org.bettermarketplace.api.dto;
 
 import java.time.Instant;
 
-import org.bettermarketplace.db.entity.FileReference;
+import org.bettermarketplace.db.entity.FileReferenceDbo;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public record OpenedFileDto(Long id, String name, String type, Instant createdAt, Instant updatedAt, byte[] content) {
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class OpenedFileDto {
-
-    private Long id;
-    private String name;
-    private String type;
-    private Instant createdAt;
-    private Instant updatedAt;
-    private byte[] content;
-
-    public static OpenedFileDto from(FileReference fileReference, byte[] content) {
-        OpenedFileDto openedFileDto = new OpenedFileDto();
-        openedFileDto.setId(fileReference.getId());
-        openedFileDto.setType(fileReference.getType());
-        openedFileDto.setCreatedAt(fileReference.getCreatedAt());
-        openedFileDto.setUpdatedAt(fileReference.getUpdatedAt());
-        openedFileDto.setName(fileReference.getName());
-        openedFileDto.setContent(content);
-        return openedFileDto;
+    public static OpenedFileDto from(FileReferenceDbo fileReference, byte[] content) {
+        return new OpenedFileDto(
+                fileReference.id(),
+                fileReference.name(),
+                fileReference.type(),
+                fileReference.createdAt(),
+                fileReference.updatedAt(),
+                content);
     }
 }
