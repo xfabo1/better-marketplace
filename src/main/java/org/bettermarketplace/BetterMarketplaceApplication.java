d51b7efff86917fbcdea46b861d31df8428378b5
xfabo1 <filip.fabry16@gmail.com>
2024-12-14 10:52:48 +0100

    wip: Implement the location dao

--- src/main/java/org/bettermarketplace/BetterMarketplaceApplication.java
+++ src/main/java/org/bettermarketplace/BetterMarketplaceApplication.java
@@ -2,6 +2,7 @@ package org.bettermarketplace;
 
 import static org.springframework.boot.SpringApplication.run;
 
+import org.bettermarketplace.service.LocationService;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
--- src/main/java/org/bettermarketplace/http/OpenDataClient.java
+++ src/main/java/org/bettermarketplace/http/OpenDataClient.java
@@ -1,5 +1,6 @@
 package org.bettermarketplace.http;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.bettermarketplace.http.dto.PostalCodeDto;
@@ -18,13 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 public interface OpenDataClient {
 
 	@GetMapping("/json?lang=en&timezone=Europe/Berlin")
-	Stream<PostalCodeDto> fetchPostalCodes(@RequestParam("refine") String refine);
+	List<PostalCodeDto> fetchPostalCodes(@RequestParam("refine") String refine);
 
 	@Slf4j
 	class Fallback implements OpenDataClient {
 
 		@Override
-		public Stream<PostalCodeDto> fetchPostalCodes(String refine) {
+		public List<PostalCodeDto> fetchPostalCodes(String refine) {
 			log.info("Could not fetch the postal codes for '{}'", refine);
 			return null;
 		}
