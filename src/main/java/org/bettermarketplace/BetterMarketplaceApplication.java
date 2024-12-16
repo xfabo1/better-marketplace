065d697d4b2736c2c935d169897274ebe6254a1a
xfabo1 <filip.fabry16@gmail.com>
2024-12-16 13:39:35 +0100

    feat: Replace the Location controller with cron job

--- src/main/java/org/bettermarketplace/BetterMarketplaceApplication.java
+++ src/main/java/org/bettermarketplace/BetterMarketplaceApplication.java
@@ -6,9 +6,11 @@ import org.bettermarketplace.service.LocationService;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class BetterMarketplaceApplication {
 
 	public static void main(String[] args) {
