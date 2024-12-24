22c70a1c2dcd40fa4ffd0806b623816756e902a0
xfabo1 <filip.fabry16@gmail.com>
2024-12-24 12:03:35 +0100

    feat: Change the location table and fill database with data

--- src/main/java/org/bettermarketplace/db/entity/LocationDbo.java
+++ src/main/java/org/bettermarketplace/db/entity/LocationDbo.java
@@ -1,25 +1,16 @@
 package org.bettermarketplace.db.entity;
 
-import org.bettermarketplace.http.dto.PostalCodeDto;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
 import lombok.Builder;
 
 @Builder
-public record LocationDbo(String id,
+public record LocationDbo(Long id,
+						  @ColumnName("postal_code")
+						  String postalCode,
 						  @ColumnName("country_code")
 						  String countryCode,
 						  String name,
 						  float latitude,
 						  float longitude) {
-
-	public static LocationDbo from(PostalCodeDto postalCodeDto) {
-		return LocationDbo.builder()
-				.id(postalCodeDto.postalCode())
-				.countryCode(postalCodeDto.countryCode())
-				.name(postalCodeDto.placeName())
-				.latitude(postalCodeDto.latitude())
-				.longitude(postalCodeDto.longitude())
-				.build();
-	}
 }
--- src/main/java/org/bettermarketplace/mapper/LocationMapper.java
+++ src/main/java/org/bettermarketplace/mapper/LocationMapper.java
@@ -2,6 +2,7 @@ package org.bettermarketplace.mapper;
 
 import org.bettermarketplace.db.entity.LocationDbo;
 import org.bettermarketplace.http.dto.PostalCodeDto;
+import org.bettermarketplace.model.Location;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -11,7 +12,6 @@ public interface LocationMapper {
 
 	LocationMapper INSTANCE = Mappers.getMapper(LocationMapper.class);
 
-	@Mapping(source = "postalCode", target = "id")
 	@Mapping(source = "placeName", target = "name")
-	LocationDbo from(PostalCodeDto postalCodeDto);
+	Location from(PostalCodeDto postalCodeDto);
 }
